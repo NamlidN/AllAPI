617ea67149f1cd6da057b3601b10815f901da54b
NamlidN <dilman.n999@gmail.com>
2023-03-24 03:27:27 +0100

    BulletAddAndFIX

--- src/Pages/SpaceGame/SpaceGame.js
+++ src/Pages/SpaceGame/SpaceGame.js
@@ -4,17 +4,25 @@ import "./SpaceGame.css";
 let SpaceSPAN;
 let SpaceMap;
 let MAP_ITEM_ELEMENTS = 1000;
-// let PlayerGun
+
 let PlayerShoot;
 let Player_Shoot_Bullet;
 let PressAnyKeySpace;
+//!!!!!!!!!!!!!!!!!!!!!!!
+// let PlayerGun
+// const [PlayerOrMap, setPlayerOrMap] = useState();
+// PlayerGun = document.createElement("span");
+// PlayerGun.classList.add("PlayerGun");
+// PlayerGun.setAttribute("id", "Playerbarrel");
+// PlayerShoot.appendChild(PlayerGun);
+//!!!!!!!!!!!!!!!!!!!!!!!
 function SpaceGame() {
   const [keyCode, setKeyCode] = useState(null);
   const [PlayerLeft, setPL] = useState(974);
   const [PlayerRight, setPR] = useState(976);
   const [Playerbarrel, setPBarrel] = useState(PlayerLeft - 49);
-  const [PlayerBullet, setPBullet] = useState();
-  // const [PlayerOrMap, setPlayerOrMap] = useState();
+  const [PlayerBullet, setPBullet] = useState(Playerbarrel - 50);
+console.log(PlayerBullet)
   function StartSpaceGame() {
     let FocusElement = document.getElementById("WorldSPace");
     PressAnyKeySpace = document.getElementById("AnyKeySpace");
@@ -41,16 +49,36 @@ function SpaceGame() {
       PlayerShoot = document.getElementById(Playerbarrel);
       PlayerShoot.classList.add("PlayerSpaceShoot");
       PlayerShoot.classList.remove("SpaceGameMap");
-
     }
-    // PlayerGun = document.createElement("span");
-    // PlayerGun.classList.add("PlayerGun");
-    // PlayerGun.setAttribute("id", "Playerbarrel");
-    // PlayerShoot.appendChild(PlayerGun);
+   
   }
   function FocusEvent() {
     PressAnyKeySpace.style.display = "none";
   }
+ 
+
+let intervalId;
+let Time = 1;
+function BullteShooterEvent() {
+  intervalId = setInterval(() => {
+    Player_Shoot_Bullet = document.getElementById(Playerbarrel - Time*100 / 2);
+    Player_Shoot_Bullet.classList.remove("SpaceGameMap");
+    Player_Shoot_Bullet.classList.add("PlayerBullet");
+    Time++;
+  }, 1000);
+}
+
+function stopInterval() {
+  if ( Time  !== 100 ) {
+    return
+	}
+  else{
+	  clearInterval(intervalId);
+
+  }
+
+}
+
   function handleKeyDown(e) {
     console.log(keyCode);
     setKeyCode(e.keyCode);
@@ -104,37 +132,35 @@ function SpaceGame() {
         // rechts
         break;
 
-        case 32:
-      //!!    WHY 2 mal tippen broooooooooooooo vIeL sPaß 
-setPBullet(Playerbarrel - 50)
-          Player_Shoot_Bullet = document.getElementById(PlayerBullet)
-          Player_Shoot_Bullet.classList.remove("SpaceGameMap");
-          Player_Shoot_Bullet.classList.add("PlayerBullet");
+      case 32:
+        //!!   Bullet tracer remove 
+        setPBullet(Playerbarrel - 50);
+        BullteShooterEvent()
+        stopInterval()
 
-          //   for (let i = PlayerLeft; i <= PlayerRight; i++) {
-          //     let Player = document.getElementById(i);
-          //     PlayerShoot = document.getElementById(Playerbarrel);
-          //     PlayerShoot.classList.remove("PlayerSpaceShoot");
-          //     PlayerShoot.classList.add("SpaceGameMap");
-          //     Player.classList.remove("PlayerSpace");
-          //     Player.classList.add("SpaceGameMap");
-          //   }
-          //   setPL((prev) => prev + 1);
-          //   setPR((prev) => prev + 1);
-          //   setPBarrel((prev) => prev + 1);
-          //   for (let i = PlayerLeft + 1; i <= PlayerRight + 1; i++) {
-          //     PlayerShoot = document.getElementById(Playerbarrel + 1);
-          //     PlayerShoot.classList.add("PlayerSpaceShoot");
-          //     PlayerShoot.classList.remove("SpaceGameMap");
-          //     let Player = document.getElementById(i);
-          //     Player.classList.remove("SpaceGameMap");
-          //     Player.classList.add("PlayerSpace");
-          //   }
-          // }
+        
+        //   for (let i = PlayerLeft; i <= PlayerRight; i++) {
+        //     let Player = document.getElementById(i);
+        //     PlayerShoot = document.getElementById(Playerbarrel);
+        //     PlayerShoot.classList.remove("PlayerSpaceShoot");
+        //     PlayerShoot.classList.add("SpaceGameMap");
+        //     Player.classList.remove("PlayerSpace");
+        //     Player.classList.add("SpaceGameMap");
+        //   }
+        //   setPL((prev) => prev + 1);
+        //   setPR((prev) => prev + 1);
+        //   setPBarrel((prev) => prev + 1);
+        //   for (let i = PlayerLeft + 1; i <= PlayerRight + 1; i++) {
+        //     PlayerShoot = document.getElementById(Playerbarrel + 1);
+        //     PlayerShoot.classList.add("PlayerSpaceShoot");
+        //     PlayerShoot.classList.remove("SpaceGameMap");
+        //     let Player = document.getElementById(i);
+        //     Player.classList.remove("SpaceGameMap");
+        //     Player.classList.add("PlayerSpace");
+        //   }
+        // }
 
-
- 
-break;
+        break;
       default:
         break;
     }
@@ -151,9 +177,9 @@ break;
         <span className="StartBTN_SPACEITEM">Start Game </span>
       </button>
       <div className="SpaceContainer" id="SpaceMap" onClick={FocusEvent}>
-      <h1 className="AnyKeySpace" id="AnyKeySpace">
-        Click to start the Game
-      </h1>
+        <h1 className="AnyKeySpace" id="AnyKeySpace">
+          Click to start the Game
+        </h1>
       </div>
     </span>
   );
